8d05c02e5f42fbf8458d307cc0fbaf62ecf28bb3
Илья Евсеенко <addminuser258@gmail.com>
2020-11-07 00:15:50 +0300

    попытка запустить сервис ворке 2

--- rasp/ip193/sw.js
+++ rasp/ip193/sw.js
@@ -1,32 +1,18 @@
-self.addEventListener('install', function(event) {
-    var indexPage = new Request('index.html');
-    event.waitUntil(
-      fetch(indexPage).then(function(response) {
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          console.log('[PWA Builder] Cached index page during Install'+ response.url);
-          return cache.put(indexPage, response);
-        });
-    }));
-  });
-  self.addEventListener('fetch', function(event) {
-    var updateCache = function(request){
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return fetch(request).then(function (response) {
-          console.log('[PWA Builder] add page to offline'+response.url)
-          return cache.put(request, response);
-        });
-      });
-    };
-    event.waitUntil(updateCache(event.request));
-    event.respondWith(
-      fetch(event.request).catch(function(error) {
-        console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-        return caches.open('pwabuilder-offline').then(function (cache) {
-          return cache.match(event.request).then(function (matching) {
-            var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-            return report
-          });
-        });
-      })
-    );
+// This is the "Offline copy of assets" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
   })
+);
