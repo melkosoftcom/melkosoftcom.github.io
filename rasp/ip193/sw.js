1df514ff07bbee1acbc64934a36585c71d8557dc
Илья Евсеенко <addminuser258@gmail.com>
2020-11-07 00:27:40 +0300

    update sw

--- rasp/ip193/sw.js
+++ rasp/ip193/sw.js
@@ -1,54 +1,37 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+const version = "0.0.1"; //тут может быть номер вашей версии
+const CACHE = "lencodigitexerrasp-cache" + version; //вместо soltyk - используйте свое уникальное название или доменное имя
+self.addEventListener('install', function(event) {
+    var indexPage = new Request('index.html');
+    event.waitUntil(fetch(indexPage).then(function(response) {
+        var response2 = response.clone();
+        return caches.open(CACHE).then(function(cache) {
+            console.log('[PWA Builder] Cached index page during Install ' + response2.url);
+            return cache.put(indexPage, response2)
+        })
+    }))
 });
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+self.addEventListener('fetch', function(event) {
+    var updateCache = function(request) {
+        return caches.open(CACHE).then(function(cache) {
+            return fetch(request).then(
+                function(response2 = response.clone()) {
+                    if (updated.indexOf(response2.url) != -1) {
+                        console.log('Исключен из кеша ' + response2.url)
+                    } else {
+                        console.log('[PWA Builder] add page to offline ' + response2.url)
+                        return cache.put(request.clone(), response2)
+                    }
+                })
+        })
+    };
+    event.waitUntil(updateCache(event.request));
+    event.respondWith(fetch(event.request).catch(function(error) {
+        console.log('[PWA Builder] Network request Failed. Serving content from cache: ' + error);
+        return caches.open(CACHE).then(function(cache) {
+            return cache.match(event.request).then(function(matching) {
+                var report = !matching || matching.status == 404 ? Promise.reject('no-match') : matching;
+                return report
+            })
+        })
+    }))
+})
